dddf90f2f4306df6d3a6903e19e1a51db9bd9852
Alexander Clare <alexander.clare@gmail.com>
2011-10-07 03:02:18 -0500

    Add bookmarks from command line
    Add bookmarks in addition to generating HTML from the template; also,
    change output method from direct-to-file to print-to-console for
    flexibility.

--- example/marks.clj
+++ example/marks.clj
@@ -1,7 +1,9 @@
+(
 {:title "Enlive"
  :link "https://github.com/cgrand/enlive"
  :tags #{:clojure}}
 {:title "Enlive Tutorial"
  :link "https://github.com/swannodette/enlive-tutorial/"
  :tags #{:clojure}}
+)
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bookmarks "1.1.0"
+(defproject bookmarks "2.0.0"
   :description "HTML bookmark page generator"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [enlive "1.0.0-SNAPSHOT"]]
