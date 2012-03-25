ee019cf8ca5a8bc3c5019603b8b50916ffcd07af
Alexander Clare <alexander.clare@gmail.com>
2012-03-25 03:47:04 -0500

    Bump to Clojure 1.3.0

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject bookmarks "2.0.0"
   :description "HTML bookmark page generator"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [enlive "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [enlive "1.0.0"]]
   :main bookmarks)
-
